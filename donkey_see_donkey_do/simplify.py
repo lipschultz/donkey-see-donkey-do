430057bbafa260c4aaf6c966229402073332f3b2
Michael Lipschultz <michael.lipschultz@gmail.com>
2023-05-27 08:45:39 -0400

    Write event comparison simplifiers & Rewrite existing simplifiers to use

--- donkey_see_donkey_do/simplify.py
+++ donkey_see_donkey_do/simplify.py
@@ -14,126 +14,162 @@ from donkey_see_donkey_do.events import (
 
 
 def drop_consecutive_state_snapshots(events: Events) -> Events:
-    """If there are consecutive snapshot events, keep only the last one."""
-    reversed_saved_events = []
-    for event in reversed(events):
-        if len(reversed_saved_events) == 0:
-            reversed_saved_events.append(event)
-        elif isinstance(event, StateSnapshotEvent):
-            if not isinstance(reversed_saved_events[-1], StateSnapshotEvent):
-                reversed_saved_events.append(event)
-        else:
-            reversed_saved_events.append(event)
+    return merge_consecutive_events(
+        events,
+        [drop_consecutive_state_snapshots_1],
+    )
 
-    return Events.from_iterable(reversed(reversed_saved_events))
+
+def drop_consecutive_state_snapshots_1(first_event: BaseEvent, second_event: BaseEvent) -> List[BaseEvent]:
+    """If both events are snapshot events, keep only the second, otherwise return both."""
+    if isinstance(first_event, StateSnapshotEvent) and isinstance(second_event, StateSnapshotEvent):
+        return [second_event]
+    return [first_event, second_event]
 
 
 def convert_mouse_press_then_release_to_click(events: Events, max_seconds: float = 0.2, max_pixels: int = 5) -> Events:
+    return merge_consecutive_events(
+        events,
+        [
+            lambda e1, e2: merge_consecutive_mouse_press_then_release_to_click(
+                e1, e2, max_seconds=max_seconds, max_pixels=max_pixels
+            )
+        ],
+    )
+
+
+def merge_consecutive_mouse_press_then_release_to_click(
+    first_event: BaseEvent, second_event: BaseEvent, max_seconds: float = 0.2, max_pixels: int = 5
+) -> List[BaseEvent]:
     """
     Convert a mouse press event followed by release event into a click event, as long as the events are consecutive,
     no more than ``max_seconds`` apart from each other, and the distance the mouse moved is no more than ``max_pixels``.
 
     The distance for mouse movement is computed using Euclidean distance.
     """
-    new_events = []
-    for event in events:
-        if (
-            len(new_events) > 0
-            and isinstance(event, MouseButtonEvent)
-            and isinstance(new_events[-1], MouseButtonEvent)
-            and new_events[-1].button == event.button
-            and new_events[-1].action == "press"
-            and event.action == "release"
-            and (event.timestamp - new_events[-1].timestamp).total_seconds() <= max_seconds
-            and (event.location.distance_to(new_events[-1].location)) <= max_pixels
-        ):
-            new_events[-1] = ClickEvent.from_mouse_button_event(new_events[-1])
-        else:
-            new_events.append(event)
+    if not isinstance(first_event, MouseButtonEvent) or not isinstance(second_event, MouseButtonEvent):
+        return [first_event, second_event]
 
-    return Events.from_iterable(new_events)
+    if first_event.timestamp > second_event.timestamp:
+        # Swap events so first_event comes first chronologically
+        first_event, second_event = second_event, first_event
+
+    if (
+        first_event.button != second_event.button
+        or first_event.action != "press"
+        or second_event.action != "release"
+        or (second_event.timestamp - first_event.last_timestamp_or_first).total_seconds() > max_seconds
+        or first_event.location.distance_to(second_event.location) > max_pixels
+    ):
+        return [first_event, second_event]
+
+    return [ClickEvent.from_mouse_button_event(first_event)]
 
 
 def convert_mouse_clicks_to_multi_click(events: Events, max_seconds: float = 0.4, max_pixels: int = 5) -> Events:
+    return merge_consecutive_events(
+        events,
+        [
+            lambda e1, e2: merge_consecutive_mouse_clicks_to_multi_click(
+                e1, e2, max_seconds=max_seconds, max_pixels=max_pixels
+            )
+        ],
+    )
+
+
+def merge_consecutive_mouse_clicks_to_multi_click(
+    first_event: BaseEvent, second_event: BaseEvent, max_seconds: float = 0.4, max_pixels: int = 5
+) -> List[BaseEvent]:
     """
     Convert sequential clicks of the same mouse button to a multi-click (e.g. double-click), as long as the subsequent
     click is at most ``max_seconds`` after the previous and the distance the mouse moved is no more than ``max_pixels``.
 
     The distance for mouse movement is computed using Euclidean distance.
     """
-    reversed_saved_events = []
-    for event in reversed(events):
-        if (
-            len(reversed_saved_events) > 0
-            and isinstance(event, (MouseButtonEvent, ClickEvent))
-            and isinstance(reversed_saved_events[-1], (MouseButtonEvent, ClickEvent))
-            and reversed_saved_events[-1].button == event.button
-            and reversed_saved_events[-1].action == "click"
-            and event.action == "click"
-            and (reversed_saved_events[-1].timestamp - event.last_timestamp_or_first).total_seconds() <= max_seconds
-            and (event.location.distance_to(reversed_saved_events[-1].location)) <= max_pixels
-        ):
-            next_event = reversed_saved_events[-1]
-            if not isinstance(next_event, ClickEvent):
-                next_event = ClickEvent.from_mouse_button_event(next_event)
+    if not isinstance(first_event, MouseButtonEvent) or not isinstance(second_event, MouseButtonEvent):
+        return [first_event, second_event]
 
-            if not isinstance(event, ClickEvent):
-                event = ClickEvent.from_mouse_button_event(event)
-            else:
-                event = event.copy()
+    if first_event.timestamp > second_event.timestamp:
+        # Swap events so first_event comes first chronologically
+        first_event, second_event = second_event, first_event
 
-            event.update_with(next_event)
+    if (
+        first_event.button != second_event.button
+        or first_event.action != "click"
+        or second_event.action != "click"
+        or (second_event.timestamp - first_event.last_timestamp_or_first).total_seconds() > max_seconds
+        or first_event.location.distance_to(second_event.location) > max_pixels
+    ):
+        return [first_event, second_event]
 
-            reversed_saved_events[-1] = event
-        else:
-            reversed_saved_events.append(event)
+    if not isinstance(first_event, ClickEvent):
+        first_event = ClickEvent.from_mouse_button_event(first_event)
 
-    return Events.from_iterable(reversed(reversed_saved_events))
+    if not isinstance(second_event, ClickEvent):
+        second_event = ClickEvent.from_mouse_button_event(second_event)
+
+    first_event = first_event.copy()
+    first_event.update_with(second_event)
+
+    return [first_event]
 
 
 def convert_key_press_then_release_to_write(events: Events, max_seconds: float = 0.15) -> Events:
+    return merge_consecutive_events(
+        events, [lambda e1, e2: merge_consecutive_key_press_then_release_to_write(e1, e2, max_seconds=max_seconds)]
+    )
+
+
+def merge_consecutive_key_press_then_release_to_write(
+    first_event: BaseEvent, second_event: BaseEvent, max_seconds: float = 0.15
+) -> List[BaseEvent]:
     """
     Convert a key press event followed by release event into a write event, as long as the events are consecutive and
     no more than ``max_seconds`` apart from each other.
     """
-    new_events = []
-    for event in events:
-        if (
-            len(new_events) > 0
-            and isinstance(event, KeyboardEvent)
-            and isinstance(new_events[-1], KeyboardEvent)
-            and new_events[-1].key == event.key
-            and new_events[-1].action == "press"
-            and event.action == "release"
-            and (event.timestamp - new_events[-1].timestamp).total_seconds() <= max_seconds
-        ):
-            new_events[-1] = WriteEvent.from_keyboard_event(new_events[-1])
-        else:
-            new_events.append(event)
+    if not isinstance(first_event, KeyboardEvent) or not isinstance(second_event, KeyboardEvent):
+        return [first_event, second_event]
 
-    return Events.from_iterable(new_events)
+    if first_event.timestamp > second_event.timestamp:
+        # Swap events so first_event comes first chronologically
+        first_event, second_event = second_event, first_event
+
+    if (
+        (second_event.timestamp - first_event.last_timestamp_or_first).total_seconds() > max_seconds
+        or first_event.key != second_event.key
+        or first_event.action != "press"
+        or second_event.action != "release"
+    ):
+        return [first_event, second_event]
+
+    return [WriteEvent.from_keyboard_event(first_event)]
 
 
 def merge_consecutive_write_events(events: Events, max_seconds: float = 1) -> Events:
+    return merge_consecutive_events(
+        events, [lambda e1, e2: merge_consecutive_write_events_1(e1, e2, max_seconds=max_seconds)]
+    )
+
+
+def merge_consecutive_write_events_1(
+    first_event: BaseEvent, second_event: BaseEvent, max_seconds: float = 1
+) -> List[BaseEvent]:
     """
     Merge consecutive write events that are no more than ``max_seconds`` apart.
     """
-    reversed_saved_events = []
-    for event in reversed(events):
-        if (
-            len(reversed_saved_events) > 0
-            and isinstance(event, WriteEvent)
-            and isinstance(reversed_saved_events[-1], WriteEvent)
-            and (reversed_saved_events[-1].timestamp - event.last_timestamp_or_first).total_seconds() <= max_seconds
-        ):
-            next_event = reversed_saved_events[-1]
-            event = event.copy(deep=True)
-            event.append(next_event)
-            reversed_saved_events[-1] = event
-        else:
-            reversed_saved_events.append(event)
+    if not isinstance(first_event, WriteEvent) or not isinstance(second_event, WriteEvent):
+        return [first_event, second_event]
 
-    return Events.from_iterable(reversed(reversed_saved_events))
+    if first_event.timestamp > second_event.timestamp:
+        # Swap events so first_event comes first chronologically
+        first_event, second_event = second_event, first_event
+
+    if (second_event.timestamp - first_event.last_timestamp_or_first).total_seconds() > max_seconds:
+        return [first_event, second_event]
+
+    event = first_event.copy(deep=True)
+    event.update_with(second_event)
+    return [event]
 
 
 def merge_consecutive_scroll_events(
